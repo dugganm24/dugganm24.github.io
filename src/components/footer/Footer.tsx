d60ccec2de119ef0fe83fd766be5422c1b5217f6
Michael Duggan <mpduggan@wpi.edu>
2024-12-30 22:41:26 -0500

    footer formatting fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,22 +8,33 @@ const Footer = () => {
     const currentYear: number = new Date().getFullYear(); // Get the current year
 
     return (
-        <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-            <div className="container flex justify-center items-center mt-4">
+        <footer className="footer border-t border-t-[#33353F] text-white py-4">
+            {/* First Row */}
+            <div className="flex justify-center items-center">
                 <p className="text-[#BFBCBC] text-lg">
                     &copy; {currentYear} Michael Duggan. All rights reserved.
                 </p>
             </div>
-            <div className="container flex justify-center items-center mb-4">
-                <Link href="https://github.com/dugganm24">
-                    <Image src={GithubIcon} alt="Github Icon" />
+
+            {/* Second Row */}
+            <div className="flex justify-center items-center gap-2 mt-2">
+                <Link href="https://github.com/dugganm24" aria-label="GitHub Profile">
+                    <Image 
+                        src={GithubIcon} 
+                        alt="GitHub Icon" 
+                        className="hover:opacity-80 transition-opacity"
+                    />
                 </Link>
-                <Link href="https://www.linkedin.com/in/mpduggan/">
-                    <Image src={LinkedinIcon} alt="Github Icon" />
+                <Link href="https://www.linkedin.com/in/mpduggan/" aria-label="LinkedIn Profile">
+                    <Image 
+                        src={LinkedinIcon} 
+                        alt="LinkedIn Icon" 
+                        className="hover:opacity-80 transition-opacity"
+                    />
                 </Link>
             </div>
         </footer>
-    )
+    );
 }
 
 export default Footer;
