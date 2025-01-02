d2c9092dc7e48ca4d90778f74e922348b90fd9bc
Michael Duggan <mpduggan@wpi.edu>
2025-01-02 15:01:04 -0500

    Update config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/Duggan-Portfolio",
   output: "export",
-  reactStrictMode: true,
 };
 
 export default nextConfig;
