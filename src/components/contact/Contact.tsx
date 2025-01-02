203ea565095b55eed10113f16163bb8b8c09ac2e
Michael Duggan <mpduggan@wpi.edu>
2025-01-02 11:10:10 -0500

    form functionality with emailJS

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,12 +1,56 @@
 "use client";
 
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import emailjs from "@emailjs/browser";
+import { motion } from "framer-motion";
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 
 const Contact = () => {
+    const [form, setForm] = useState({ email: "", subject: "", message: "" });
+    const [status, setStatus] = useState(""); // Feedback message
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { id, value } = e.target;
+        setForm({ ...form, [id]: value });
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setStatus("Sending...");
+
+        emailjs
+            .send(
+                "service_u8p4yzd", // Service ID
+                "template_2b3b627", // Template ID
+                {
+                    email: form.email,
+                    subject: form.subject,
+                    message: form.message,
+                },
+                "1k4I5tqEcGLPeHav9" // Public Key
+            )
+            .then(
+                () => {
+                    setStatus("Message sent successfully!");
+                    setForm({ email: "", subject: "", message: "" }); // Reset form
+                },
+                (error) => {
+                    console.error("Failed to send message:", error);
+                    setStatus("Failed to send message. Please try again.");
+                }
+            );
+    };
+
+    useEffect(() => {
+        if (status) {
+            const timeout = setTimeout(() => setStatus(""), 5000);
+            return () => clearTimeout(timeout); // Cleanup timeout
+        }
+    }, [status]);
+
     return (
         <section className="grid md:grid-cols-2 py-8 gap-4">
             <div>
@@ -14,7 +58,6 @@ const Contact = () => {
                     Let's Connect
                 </h2>
                 <p className="text-[#BFBCBC] mb-4 max-w-md">
-                    {" "}
                     As an upcoming new college grad, I’m currently looking for entry-level software engineering positions. My inbox is always open, don’t hesitate to reach out.
                 </p>
                 <div className="socials flex flex-row gap-2">
@@ -22,66 +65,88 @@ const Contact = () => {
                         <Image src={GithubIcon} alt="Github Icon" />
                     </Link>
                     <Link href="https://www.linkedin.com/in/mpduggan/">
-                        <Image src={LinkedinIcon} alt="Github Icon" />
+                        <Image src={LinkedinIcon} alt="Linkedin Icon" />
                     </Link>
                 </div>
             </div>
             <div>
-                <form className="flex flex-col">
+                <form className="flex flex-col" onSubmit={handleSubmit}>
                     <div className="mb-6">
-                        <label 
-                            htmlFor="email" 
+                        <label
+                            htmlFor="email"
                             className="text-white block mb-2 text-md font-semibold"
                         >
                             Your email
                         </label>
-                    <input 
-                            type="email" 
-                            id="email" 
+                        <input
+                            type="email"
+                            id="email"
+                            value={form.email}
+                            onChange={handleChange}
                             required
                             className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                             placeholder="michael@google.com"
                         />
                     </div>
                     <div className="mb-6">
-                        <label 
-                            htmlFor="subject" 
+                        <label
+                            htmlFor="subject"
                             className="text-white block mb-2 text-md font-semibold"
                         >
                             Subject
                         </label>
-                        <input 
-                            type="text" 
+                        <input
+                            type="text"
                             id="subject"
+                            value={form.subject}
+                            onChange={handleChange}
                             required
                             className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                             placeholder="Just saying hi"
                         />
                     </div>
                     <div className="mb-6">
-                        <label  
+                        <label
                             htmlFor="message"
                             className="text-white block mb-2 text-md font-semibold"
                         >
                             Message
                         </label>
-                        <textarea 
-                            name="message"
+                        <textarea
                             id="message"
+                            value={form.message}
+                            onChange={handleChange}
+                            required
                             className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                             placeholder="Let's talk about..."
                         />
                     </div>
                     <button
                         type="submit"
-                        className="bg-[#00FFC8] bg-opacity-80 hover:scale-105 text-white fond-medium py-2.5 px-5 rounded-lg w-full"
+                        className="bg-[#00FFC8] bg-opacity-80 hover:scale-105 text-white font-medium py-2.5 px-5 rounded-lg w-full"
                     >
-                            Send Message
-                        </button>
+                        Send Message
+                    </button>
                 </form>
+                {status && (
+                    <motion.div
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        className={`mt-4 p-4 rounded-lg text-center ${
+                            status === "Sending..."
+                                ? "bg-gray-500 text-white"
+                                : status === "Message sent successfully!"
+                                ? "bg-green-500 text-white"
+                                : "bg-red-500 text-white"
+                        }`}
+                    >
+                        <span>{status}</span>
+                    </motion.div>
+                )}
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Contact;
