2cc7fddce8f0c0e7d35c7a08eaf623ab028da251
Michael Duggan <mpduggan@wpi.edu>
2025-04-11 16:06:59 -0400

    slightly larger tags on mobile screen

--- src/components/projects/ProjectTag.tsx
+++ src/components/projects/ProjectTag.tsx
@@ -12,7 +12,7 @@ const ProjectTag: React.FC<ProjectTagProps> = ({ tag, onClick, isSelected }) => 
     ? "text-white border-[#00FFC8] hover:scale-105"
     : "text-[#BFBCBC] border-slate-600 hover:scale-105 hover:text-white";
     return (
-        <button className={`${buttonStyles} rounded-full border-2 px-4 py-2 text-sm sm:px-6 sm:py-3 sm:text-xl cursor-pointer`}
+        <button className={`${buttonStyles} rounded-full border-2 px-4 py-2 text-md sm:px-6 sm:py-3 sm:text-xl cursor-pointer`}
                 onClick={() => onClick(tag)}
         >
             {tag}
